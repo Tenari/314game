976b0df15f334fb33af81a4d41de94b64a36ef0b
Tenari the Great <djz24@case.edu>
2013-04-04 13:24:23 -0400

    Started padding out first scene.

--- game.s
+++ game.s
@@ -2,12 +2,21 @@
 	.globl  main
 main:   
 	#start code----------------------------------------------------------
+		# Set the scene, and offer the player the chance to answer the door.
 		la		$a0, Dorm1			#load the argument 'Dorm1' into $a0
 		jal		Scene				#print and get response in $v0
 
+		# If they said yes, they win. else, ask them again.
 		addi	$t1, $zero, 1		# put 1 into $t1
 		beq		$v0, $t1, win1		# if response = 1, jump to win1
-		j 		lose1				# else, jump to lose1
+		
+		# Ask them again.
+		la		$a0, KnockAgain		# Load appropriate message into argument $a0 for Scene function
+		jal		Scene				# Guy knocks again. What do you do? $v0 holds int answer.
+		
+		# They either said yes or they lose.
+		beq		$v0, $t1, win1		# $t1 still - 1. Thus, if response = $t1, jump to win1
+		j 		lose1				# else, jump to lose1, because the player never answered the door.
 		
 	win1:	
 		# system call to print win message 
@@ -26,7 +35,7 @@ main:
 
 #-------------------------------------------------------------------------	
 #The scene function prints the standard scene message (adress passed in to $a0) and asks for user response
-#$v0 SceneInt($a0 = msgAddress){} where $v0 = user response input as an int.
+#$v0 SceneInt($a0 = msgAddress) where $v0 = user response input as an int.
 #note that $a0 is changed by operation.
 Scene:
 	# system call to print message stored at $a0
@@ -55,6 +64,7 @@ end:
         .data
 Prompt:   	.asciiz "\nWell? => "
 Dorm1:		.asciiz "You are bored, sitting at your desk, thinking about playing another game of LoL. College isn't all it was cracked up to be. Suddenly, a knock at the door. Answer it?\n[yes = 1, no = 2]\n"
+KnockAgain:	.asciiz "\nYou ignore the knocking, being too busy staring at the wall. Soon, you hear another, louder knock at the door. Answer it?\n[yes = 1, no = 2]\n"
 Lose1:		.asciiz "\nYou continue sitting at your desk. The knocking subsides, and you are left with the vague feeling that you just missed the opportunity of a lifetime.\nYOU LOSE!"
 Win1:		.asciiz "\nYou answer the door, and go on to have grand adventures.\nYOU WIN!"
 
